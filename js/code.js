6a064b475776a40aee71b33c83c922c57e0a94b7
Jorge Perez <jorge-andresperez@hotmail.com>
2022-08-06 16:42:33 -0500

    arrays

--- js/code.js
+++ js/code.js
@@ -24,8 +24,6 @@ if(Number.isNaN(periodos)){
 //Funciones base
 let interesper = (int,val) => val*int
 let valamort = (val,per) => val/per
-let cuotaa = (intper,valamo) => interper+valamo
-let montor = (montoant,cuotaact) => montoant-cuotaact
     //El periodo lo calcularemos con el ciclo
 
 //Cálculos y resultados
@@ -34,6 +32,7 @@ let montor = (montoant,cuotaact) => montoant-cuotaact
     let interesperiodo = 0
     let valoramortizado = 0
     let cuotaactual = 0
+    let interesestotales = []
     //let per = 0
     alert("         Datos iniciales \nMonto inicial: "+monto+"\nTasa de interés: "+interes+"\nCantidad de periodos: "+periodos)
     console.log("Periodo\tIntereses\tAmortización\tCuota\tRestante")
@@ -41,10 +40,12 @@ let montor = (montoant,cuotaact) => montoant-cuotaact
     valoramortizado = valamort(monto,periodos)
     for (let i = 1; i <= periodos; i++) {
         //const element = array[i];
-        interesperiodo = interesper(monto0,interes)
+        interesperiodo = interesper(monto,interes)
         cuotaactual = valoramortizado + interesperiodo
         monto = monto - valoramortizado
+        interesestotales.push(interesperiodo)
         //console.log("Periodo\tIntereses\tAmortización\tCuota\tRestante\n"+
         console.log(i+"\t"+interesperiodo+"\t"+valoramortizado+"\t"+cuotaactual+"\t"+monto);
         
     }
+    console.log("Se va a pagar en intereses: "+interesestotales.reduce((partialsum, a) => partialsum + a, 0))
