b580a4fa426cb1bf42abf0b6b3168f40ce30f9d2
Jorge Perez <jorge-andresperez@hotmail.com>
2022-08-10 17:38:46 -0500

    primera entrega

--- js/code.js
+++ js/code.js
@@ -1,25 +1,34 @@
 //Script para calcular amortizaciones
 
 //Declaración de variables con prompt
-let monto = parseFloat(prompt("Ingresa tu monto total a prestar"))
-let interes = parseFloat(prompt("Ingresa la tasa de interés acordada"))
-let periodos = parseInt(prompt("Ingresa la cantidad de periodos acordados"))
+// let montosim = 0 //parseFloat(prompt("Ingresa tu monto total a prestar"))
+// let interessim = 0 //parseFloat(prompt("Ingresa la tasa de interés acordada"))
+// let periodossin = 0 //parseInt(prompt("Ingresa la cantidad de periodos acordados"))
 //alert(monto) Validación de monto en vacio
 
-//Validación o comprobación de vacios en el prompt
+//Recolectar datos desde formulario
+function ingresoDatos(){
+    let montosim = document.getElementById("monto").value
+    let interessim = document.getElementById("int").value
+    let periodossin = document.getElementById("per").value
+
+    //Validación o comprobación de vacios en el prompt
 //Val para monto
-if (Number.isNaN(monto)){
-    monto = 12000000
+if (Number.isNaN(montosim)){
+    montosim = 12000000
     //alert(monto) prueba para verificar ciclo
 }
 //Val para intereses
-if(Number.isNaN(interes)){
-    interes=0.02
+if(Number.isNaN(interessim)){
+    interessim=0.02
 }
 //Val para periodos
-if(Number.isNaN(periodos)){
-    periodos=12
+if(Number.isNaN(periodossin)){
+    periodossin=12
 }
+
+//Creando el objeto
+let simu = {monto: montosim, interes: interessim, periodo:periodossin};
 
 //Funciones base
 let interesper = (int,val) => val*int
@@ -28,24 +37,74 @@ let valamort = (val,per) => val/per
 
 //Cálculos y resultados
     //datos iniciales
-    //let monto0 = monto
+    let monto0 = simu.monto
     let interesperiodo = 0
     let valoramortizado = 0
     let cuotaactual = 0
     let interesestotales = []
     //let per = 0
-    alert("         Datos iniciales \nMonto inicial: "+monto+"\nTasa de interés: "+interes+"\nCantidad de periodos: "+periodos)
+    alert("Datos iniciales \nMonto inicial: "+simu.monto+"\nTasa de interés: "+simu.interes+"\nCantidad de periodos: "+simu.periodo);
     console.log("Periodo\tIntereses\tAmortización\tCuota\tRestante")
-    console.log(0+"\t"+interesperiodo+"\t"+valoramortizado+"\t"+cuotaactual+"\t"+monto)
-    valoramortizado = valamort(monto,periodos)
-    for (let i = 1; i <= periodos; i++) {
+    console.log(0+"\t"+interesperiodo+"\t"+valoramortizado+"\t"+cuotaactual+"\t"+simu.monto)
+    valoramortizado = valamort(simu.monto,simu.periodo)
+    for (let i = 1; i <= simu.periodo; i++) {
         //const element = array[i];
-        interesperiodo = interesper(monto,interes)
+        interesperiodo = interesper(monto0,simu.interes)
         cuotaactual = valoramortizado + interesperiodo
-        monto = monto - valoramortizado
+        monto0 = monto0 - valoramortizado
         interesestotales.push(interesperiodo)
         //console.log("Periodo\tIntereses\tAmortización\tCuota\tRestante\n"+
-        console.log(i+"\t"+interesperiodo+"\t"+valoramortizado+"\t"+cuotaactual+"\t"+monto);
+        console.log(i+"\t"+interesperiodo+"\t"+valoramortizado+"\t"+cuotaactual+"\t"+monto0);
         
     }
     console.log("Se va a pagar en intereses: "+interesestotales.reduce((partialsum, a) => partialsum + a, 0))
+
+};
+
+// //Validación o comprobación de vacios en el prompt
+// //Val para monto
+// if (Number.isNaN(montosim)){
+//     montosim = 12000000
+//     //alert(monto) prueba para verificar ciclo
+// }
+// //Val para intereses
+// if(Number.isNaN(interessim)){
+//     interessim=0.02
+// }
+// //Val para periodos
+// if(Number.isNaN(periodossin)){
+//     periodossin=12
+// }
+
+// //Creando el objeto
+// let simu = {monto: montosim, interes: interessim, periodo:periodossin};
+// console.log(simu)
+
+// //Funciones base
+// let interesper = (int,val) => val*int
+// let valamort = (val,per) => val/per
+//     //El periodo lo calcularemos con el ciclo
+
+// //Cálculos y resultados
+//     //datos iniciales
+//     //let monto0 = monto
+//     let interesperiodo = 0
+//     let valoramortizado = 0
+//     let cuotaactual = 0
+//     let interesestotales = []
+//     //let per = 0
+//     alert("Datos iniciales \nMonto inicial: "+simu.monto+"\nTasa de interés: "+simu.interes+"\nCantidad de periodos: "+simu.periodo);
+//     console.log("Periodo\tIntereses\tAmortización\tCuota\tRestante")
+//     console.log(0+"\t"+interesperiodo+"\t"+valoramortizado+"\t"+cuotaactual+"\t"+monto)
+//     valoramortizado = valamort(monto,periodos)
+//     for (let i = 1; i <= periodos; i++) {
+//         //const element = array[i];
+//         interesperiodo = interesper(monto,interes)
+//         cuotaactual = valoramortizado + interesperiodo
+//         monto = monto - valoramortizado
+//         interesestotales.push(interesperiodo)
+//         //console.log("Periodo\tIntereses\tAmortización\tCuota\tRestante\n"+
+//         console.log(i+"\t"+interesperiodo+"\t"+valoramortizado+"\t"+cuotaactual+"\t"+monto);
+        
+//     }
+//     console.log("Se va a pagar en intereses: "+interesestotales.reduce((partialsum, a) => partialsum + a, 0))
